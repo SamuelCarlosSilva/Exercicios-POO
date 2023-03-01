2cf6064c6ad6489c01e4c785194055d505f7b57e
Samuel Carlos da Silva <samuel.silva@mundiale.com.br>
2023-02-28 21:42:47 -0300

    First commit

---
+++ Exercício 01 Conceitos básicos de POO/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Pizza pizza = new Pizza("calabresa");
+        pizza.adcionarIngredientes(8);
+        System.out.println(pizza.gerarNotaFiscal());
+    }
+}
---
+++ Exercício 01 Conceitos básicos de POO/Pizza.java
@@ -0,0 +1,56 @@
+public class Pizza {
+    //#region
+    
+    private final int MAX_ADCIONAIS = 8;
+    private final int VALOR_BASE = 25;
+    private final int VALOR_ADCIONAL = 4;
+    private int quantidadeIngredienteAdcional;
+    private String descricao;
+
+    private void init(String descricao){
+        this.descricao = descricao;
+        quantidadeIngredienteAdcional = 0;
+    }
+
+    public Pizza(String descricao){
+        init(descricao);
+    }
+
+    public Pizza(){
+        init("Calabresa");
+    }
+
+    /**
+     * Adciona ingredientes na pizza, máximo de 8 ingredientes adcionais por pizza.
+     * @param quantidade de ingrdiente adcionais
+     * @return Um numero inteiro
+     */
+    public int adcionarIngredientes(int quantidade){
+        if(quantidade < 0){
+            quantidadeIngredienteAdcional = 0;
+        }
+        else if(quantidade <= MAX_ADCIONAIS){
+            quantidadeIngredienteAdcional = quantidade;
+        }
+        return quantidadeIngredienteAdcional;
+    }
+
+
+    /**
+     * Calcula o valor total da pizza, realiza o calculo baseado no valor base e na quantidade de ingredientes adcionais
+     * @return Um numero inteiro
+     */
+    public int calcularValorTotal(){
+        return (VALOR_ADCIONAL * quantidadeIngredienteAdcional + VALOR_BASE);
+    }
+
+    /**
+     * Retorna a nota fiscal contendo a descricao e o valor total
+     * @return Uma String no formato Nora Fiscal/Descricao: {descricao}/ Valor total:{valor total}
+     */
+    public String gerarNotaFiscal(){
+       int valorTotal = calcularValorTotal();
+        String notaFiscal = "Nota Fiscal \nDescricao: "+descricao+"\nValor total: "+valorTotal;
+        return notaFiscal;
+    }
+}
