974cb0e1cf8213b22bbbc10308548aeb126082ff
samuel <samuelcarlos181@gmail.com>
2023-02-28 21:57:42 -0300

    new changes

--- Exercício 01 Conceitos básicos de POO/App.java
+++ Exercício 01 Conceitos básicos de POO/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Pizza pizza = new Pizza("calabresa");
-        pizza.adcionarIngredientes(7);
+        pizza.adcionarIngredientes(8);
         System.out.println(pizza.gerarNotaFiscal());
     }
 }
